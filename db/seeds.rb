be3e3a954f2b69c1d32776fac4e05b3979ba9196
Jason Swett <jason@benfranklinlabs.com>
2018-06-25 16:42:01 -0400

    Add seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,47 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+land_boat = Product.create!(
+  name: 'Land Boat',
+  price: 75000000
+)
+
+hot_tub_bike = Product.create!(
+  name: 'Hot Tub Bike',
+  price: 200000
+)
+
+disposable_single_use_grand_piano = Product.create!(
+  name: 'Disposable Single-Use Grand Piano',
+  price: 5500000
+)
+
+edible_shoes = Product.create!(
+  name: 'Edible Shoes',
+  price: 3500
+)
+
+wifi_enabled_thimble = Product.create!(
+  name: 'Wifi-Enabled Thimble',
+  price: 4500
+)
+
+InventoryAdjustment.create!([
+  {
+    product: land_boat,
+    quantity: 8
+  },
+  {
+    product: hot_tub_bike,
+    quantity: 28
+  },
+  {
+    product: disposable_single_use_grand_piano,
+    quantity: 100
+  },
+  {
+    product: edible_shoes,
+    quantity: 43
+  },
+  {
+    product: wifi_enabled_thimble,
+    quantity: 70
+  },
+])
