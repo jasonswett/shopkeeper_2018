db8bc6940c416eeb688ca58b99eb2febfdbd56b7
Jason Swett <jason@benfranklinlabs.com>
2018-06-25 15:30:40 -0400

    Add specs for Product.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,4 @@
 class Product < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
+  validates :price, presence: true
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'when name is missing' do
+    it 'is not valid' do
+      product = Product.new(price: 100)
+      expect(product).not_to be_valid
+    end
+  end
+
+  context 'when name is not unique' do
+    it 'is not valid' do
+      Product.create!(name: 'Horse', price: 50000)
+      product = Product.new(name: 'Horse', price: 50000)
+      expect(product).not_to be_valid
+    end
+  end
+
+  context 'when price is missing' do
+    it 'is not valid' do
+      product = Product.new(name: 'Horse')
+      expect(product).not_to be_valid
+    end
+  end
 end
