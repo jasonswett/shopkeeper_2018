2cdb7741fa62e97070eb1b4eeb67a7ff360684bb
Jason Swett <jason@benfranklinlabs.com>
2018-07-10 19:53:09 -0400

    Get request working.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  skip_before_action :verify_authenticity_token
+  before_action :set_default_response_format
+
+  def set_default_response_format
+    request.format = :json
+  end
 end
